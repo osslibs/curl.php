ab993ab4f17e3c63722d19f887b8f26eb4991261
Terrence Howard <chemisus@gmail.com>
2022-02-04 13:39:50 -0500

    initial commit

---
+++ src/Curl.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace osslibs\Curl;
+
+class Curl
+{
+    private $resource;
+
+    public function __construct()
+    {
+        $this->resource = curl_init();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function copy_handle()
+    {
+        return new CurlFacadeHandler(curl_copy_handle($this->resource));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setopt($option, $value)
+    {
+        return curl_setopt($this->resource, $option, $value);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setopt_array(array $options)
+    {
+        return curl_setopt_array($this->resource, $options);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function exec()
+    {
+        return curl_exec($this->resource);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getinfo($opt = null)
+    {
+        return curl_getinfo($this->resource, $opt);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function error()
+    {
+        return curl_error($this->resource);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function errno()
+    {
+        return curl_errno($this->resource);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function escape($str)
+    {
+        return curl_escape($this->resource, $str);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function close()
+    {
+        curl_close($this->resource);
+        $this->resource = null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function pause($bitmask)
+    {
+        return curl_pause($this->resource, $bitmask);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reset()
+    {
+        return curl_reset($this->resource);
+    }
+}
---
+++ tests/CurlTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace osslibs\Curl;
+
+use PHPUnit\Framework\TestCase;
+
+class CurlTest extends TestCase
+{
+    public function testHeaders()
+    {
+        $headers = ['aA: bB', 'cC: dD'];
+        $expect = ['aA' => 'bB', 'cC' => 'dD'];
+
+        $curl = new Curl();
+        $curl->setopt(CURLOPT_CUSTOMREQUEST, "GET");
+        $curl->setopt(CURLOPT_URL, "http://headers.jsontest.com/");
+        $curl->setopt(CURLOPT_HTTPHEADER, $headers);
+        $curl->setopt(CURLOPT_RETURNTRANSFER, 1);
+
+        $data = $curl->exec();
+
+        $this->assertLessThan(300, $curl->getinfo(CURLINFO_RESPONSE_CODE));
+        $this->assertArraySubset($expect, (array)json_decode($data));
+    }
+
+    public function testBody()
+    {
+        $text = json_encode((object)['a' => 'A', 'b' => 'B']);
+        $expect = ['original' => $text, 'md5' => md5($text)];
+
+        $curl = new Curl();
+        $curl->setopt(CURLOPT_CUSTOMREQUEST, "POST");
+        $curl->setopt(CURLOPT_URL, "http://md5.jsontest.com/");
+        $curl->setopt(CURLOPT_RETURNTRANSFER, 1);
+        $curl->setopt(CURLOPT_POSTFIELDS, 'text=' . $text);
+
+        $data = $curl->exec();
+
+        $this->assertLessThan(300, $curl->getinfo(CURLINFO_RESPONSE_CODE));
+        $this->assertArraySubset($expect, (array)json_decode($data));
+    }
+
+    public function testCannotConnect()
+    {
+        $text = json_encode((object)['a' => 'A', 'b' => 'B']);
+        $expect = ['original' => $text, 'md5' => md5($text)];
+
+        $curl = new Curl();
+        $curl->setopt(CURLOPT_CUSTOMREQUEST, "POST");
+        $curl->setopt(CURLOPT_URL, "badscheme://badsub.badhost.badtld/badpath");
+        $curl->setopt(CURLOPT_RETURNTRANSFER, 1);
+
+        $data = $curl->exec();
+
+        $this->assertSame(false, $data);
+        $this->assertSame(0, $curl->getinfo(CURLINFO_RESPONSE_CODE));
+    }
+
+    public function testTimeout()
+    {
+        $text = json_encode((object)['a' => 'A', 'b' => 'B']);
+        $expect = ['original' => $text, 'md5' => md5($text)];
+
+        $curl = new Curl();
+        $curl->setopt(CURLOPT_CUSTOMREQUEST, "POST");
+        $curl->setopt(CURLOPT_URL, "badscheme://badsub.badhost.badtld/badpath");
+        $curl->setopt(CURLOPT_RETURNTRANSFER, 1);
+        $curl->setopt(CURLOPT_TIMEOUT_MS, 5);
+
+        $data = $curl->exec();
+
+        $this->assertSame(false, $data);
+        $this->assertSame(0, $curl->getinfo(CURLINFO_RESPONSE_CODE));
+    }
+}
